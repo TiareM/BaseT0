8337a707a19e978009d418a461a36a4fbf29ad87
Tiare Marin <tiare.marn@uc.cl>
2024-08-24 19:51:42 -0400

    actualizado

--- src/lrsh/main.c
+++ src/lrsh/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <signal.h> 
 #include <sys/types.h>
 #include "../input_manager/manager.h"
 
@@ -11,25 +12,78 @@ bool string_equals(char *string1, char *string2) {
   return !strcmp(string1, string2);
 }
 
+/* Funcion que se ejecuta al recibir la señal*/
+void sigintHandler(int sig_num) 
+{ 
+    printf("\nPrueba\n"); 
+    exit(0);
+} 
+
 int main(int argc, char const *argv[])
 {
   int status = 1;
+  // int procesos[16];
+  // int indice_procesos = 0;
+
+  // Funcion que asigna a la señal SIGINT la fucion a ejecutar
+  signal(SIGINT, sigintHandler);
 
   do {
     char** input = read_user_input();
-    printf("%s\n", input[0]);
+    // printf("%s\n", input[0]);
 
 
-    if (string_equals(input[0], "lrexec hello")) {
+    if (string_equals(input[0], "hello")) {
       pid_t pid = fork();
       if (pid == 0) {
-        printf("Hello World!");
+        printf("Hello World!\n");
       }
     }
 
-    else if (string_equals(input[0], "lrexec sum")) {
-      printf("%s", input[1]);
+    else if (string_equals(input[0], "sum")) {
+      pid_t pid = fork();
+      if (pid == 0) {
+        float sum = atof(input[1]) + atof(input[2]);
+        printf("%f\n", sum);
+      }
     }
+
+    else if (string_equals(input[0], "is_prime")) {
+
+      pid_t pid = fork();
+      if (pid == 0) {
+
+        bool primo = true;
+        int div;
+
+        for (int i = 2; i <= atoi(input[1]) / 2; i++) {
+          div = atoi(input[1]) % i;
+          if (div == 0) {
+            primo = false;
+            break;
+          }
+        }
+
+        if (primo) {
+          printf("True\n");
+        } else {
+        printf("False\n"); 
+        }
+      }
+    }
+
+    else if (string_equals(input[0], "lrexec")) {
+
+    }
+
+    else if (string_equals(input[0], "lrlist")) {
+
+    }
+
+    else if (string_equals(input[0], "lrexit")) {
+      free_user_input(input);
+      exit(0);
+    }    
 
   free_user_input(input);
   } while (status);
