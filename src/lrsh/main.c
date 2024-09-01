b41029f68befd7cde8ae35420f38a84776096dc3
Melanie Castillo <mmcastillo1@uc.cl>
2024-09-01 16:57:27 -0400

    add: lrexec function and prevent zombies

--- src/lrsh/main.c
+++ src/lrsh/main.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <signal.h> 
 #include <sys/types.h>
+#include <sys/wait.h>
 #include "../input_manager/manager.h"
 
 // syscalls vistas en clase, como kill, exec, fork, y wait/waitpid
@@ -26,7 +27,7 @@ bool string_equals(char *string1, char *string2) {
 /* Funcion que se ejecuta al recibir la señal*/
 void sigintHandler(int sig_num) 
 { 
-    printf("\nPrueba\n"); 
+    printf("SENAL RECIBIDA %i\n", sig_num);
     exit(0);
 } 
 
@@ -48,6 +49,9 @@ int main(int argc, char const *argv[])
       pid_t pid = fork();
       if (pid == 0) {
         printf("Hello World!\n");
+        exit(0);  
+      } else if (pid > 0) {
+        waitpid(pid, NULL, 0);  
       }
     }
 
@@ -56,6 +60,9 @@ int main(int argc, char const *argv[])
       if (pid == 0) {
         float sum = atof(input[1]) + atof(input[2]);
         printf("%f\n", sum);
+        exit(0);
+      } else if (pid > 0) {
+        waitpid(pid, NULL, 0);  
       }
     }
 
@@ -77,13 +84,43 @@ int main(int argc, char const *argv[])
 
         if (primo) {
           printf("True\n");
+          exit(0);  
         } else {
-        printf("False\n"); 
+          printf("False\n"); 
+          exit(0);
         }
+      }
+      else if (pid > 0) {
+        waitpid(pid, NULL, 0);  
       }
     }
 
     else if (string_equals(input[0], "lrexec")) {
+
+      //creando mi array de argumentos
+      int argc = 0;
+      while (input[argc + 1] != NULL) { 
+          argc++;
+      }
+
+      char** args = (char**)malloc((argc + 1) * sizeof(char*)); 
+      args[0] = input[1]; // el ejecutable
+      for (int i = 0; i < argc; i++) {
+          args[i + 1] = input[i + 2]; 
+      }
+
+      pid_t pid = fork();
+      switch(pid){
+        case 0:
+          execv(args[0], args);
+          perror("execv error");
+          exit(1);
+
+        default:
+          waitpid(pid, NULL, 0); 
+      }
+          
+      free(args);
 
     }
 
@@ -100,3 +137,7 @@ int main(int argc, char const *argv[])
   } while (status);
   
 }
+
+// referencia: https://www.geeksforgeeks.org/zombie-processes-prevention/ (para zombies)
+// referencias: https://www.youtube.com/watch?v=Cn2XU9xgKHQ&list=PLssyEsGaMLoE-9UNrwZyl0J5KdhMSm1NV&index=3 (para senales)
+// referencias: https://www.youtube.com/watch?v=NHkDL0EUKbQ&list=PLssyEsGaMLoE-9UNrwZyl0J5KdhMSm1NV&index=5 (para el lrexec)
